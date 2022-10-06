ccde012dc4f59dc5958b785f6a120fabba3bf240
xfabbzz <105086288+xfabbzz@users.noreply.github.com>
2022-10-05 22:52:34 -0600

    Add .py file via upload
    project001: madlib

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+#madlib project
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input ("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"¡Hacer hiking los domingos es muy {adj}! Me gusta mucho ir a caminar los domingos porque \
+me encanta hacer {verb1}. ¡Se requiere de preparación física y mental para {verb2} como si uno fuera {famous_person}!"
+
+print(madlib)
